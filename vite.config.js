944a31c42b15c8173f641831213c69499c6c1bb9
Vladislav Orlov <me@orlov-vo.ru>
2022-09-15 10:50:02 +0300

    change port for vite dev server

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
     plugins: [sveltekit()],
+    server: { host: true, port: 8000 },
 };
 
 export default config;
