e22921f10e5331b0f14575ce312a7f63d39559bc
Vladislav Orlov <me@orlov-vo.ru>
2022-09-15 10:58:57 +0300

    remove dead file

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// Styles
-import './normalize.css';
-import './variables.css';
-import './typography.css';
-import './layout.css';
-import './blocks/post-list-short.css';
-import './blocks/post.css';
-import './blocks/feedback.css';
